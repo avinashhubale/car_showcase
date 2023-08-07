5cf45cd48e99b8f3a851dc00e6536ad595db2052
avinash hubale <avinashhubale1@gmail.com>
2023-08-08 00:07:14 +0530

    Layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,6 +19,7 @@ const Hero = () => {
 
         <CustomButton
           title="Explore Cars"
+          btnType="button"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
@@ -26,8 +27,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
