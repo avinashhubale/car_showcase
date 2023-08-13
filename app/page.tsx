c88e96d5f796c7e8bd3bef90efde30e8ab6ae4ef
avinash hubale <avinashhubale1@gmail.com>
2023-08-13 09:45:13 +0530

    Error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { fetchCars } from "@/utils";
 import { Container } from "postcss";
 import { fuels, manufacturers, yearsOfProduction } from "@/constants";
 
-export default async function Home({ searchParams }) {
+export default async function Home({ searchParams }: { searchParams: any }) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
